2d3557961bb18a5ef335e7441b5f0c719b92165e
Janita Omdahl Reiraa <janitaomdahlreiraa@gmail.com>
2026-03-04 20:01:02 +0100

    show images, name and patronus

--- uke 10/1.js
+++ uke 10/1.js
@@ -1,11 +1,28 @@
-console.log("1");
-console.log("2");
-getHarryPotterCharacters();
-console.log("3");
-console.log("4");
-
 async function getHarryPotterCharacters() {
-  const respons = await fetch("https://hp-api.onrender.com/api/characters");
-  const data = await respons.json();
-  console.log(data);
+  try {
+    const respons = await fetch("https://hp-api.onrender.com/api/characters");
+    const data = await respons.json();
+
+    return data;
+  } catch (error) {
+    console.log("klarte ikke å hente karakterer");
+  }
 }
+
+async function displayCharacters() {
+  const characters = await getHarryPotterCharacters();
+
+  console.log(characters);
+
+  characters.forEach((character) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `<img src="${character.image}" style="width: 200px"/>
+    <h2>${character.name}</2>
+    <h3>${character.patronus}</h3>`;
+
+    document.body.append(div);
+  });
+}
+
+displayCharacters();
